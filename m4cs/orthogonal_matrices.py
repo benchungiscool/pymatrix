0ac971daf0b3120f185a2ba77a2cb9421cc6de8f
Ben Chung <ben@benchung.dev>
2022-08-04 06:11:11 +0100

    Finish orthogonal matrices file

--- m4cs/orthogonal_matrices.py
+++ m4cs/orthogonal_matrices.py
@@ -6,6 +6,7 @@ from src.vectors import *
 from tests.helper import *
 from m4cs.adjugate_matrices import *
 from m4cs.matrix_inversion import *
+from math import sqrt
 
 
 #### ORTHOGONAL MATRICES ####
@@ -14,13 +15,4 @@ def is_singular(a):
   return determinant(a) == 0
 
 def is_orthogonal(a):
-  return inverse_matrix(a) == matrix_transpose(a)
-
-if __name__ == "__main__":
-  a = [
-      [2, -2, 1],
-      [1, 2, 2],
-      [2, 1, -2],
-  ]
-  print(is_orthogonal(a))
-
+  return is_identity_matrix(matrix_multiplication(a, matrix_transpose(a)))
