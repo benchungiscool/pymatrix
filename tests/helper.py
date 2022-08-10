047c0f44518e468d28315e8a6451910d70f68f1b
Ben Chung <ben@benchung.dev>
2022-08-10 22:29:12 +0100

    Add a function for printing a complete matrix

--- tests/helper.py
+++ tests/helper.py
@@ -8,5 +8,12 @@ def print_matrix(a):
     print(column_vector)
   print("-" * 3, "END PRINT MATRIX", "-" * 3)
 
+def print_complete_matrix(a):
+  for row in a:
+    val = row.pop(-1)
+    row.append("|")
+    row.append(val)
+  print_matrix(a)
+
 def print_mxn(a):
   print(f"\n-- Matrix M*N --\nm = {len(a)}\nn = {len(a[0])}\n")
