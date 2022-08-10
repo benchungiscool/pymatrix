45415a7be19ca858787edbcbad4e5d6d2be43454
Ben Chung <ben@benchung.dev>
2022-08-10 22:28:48 +0100

    Add special case for m=1 vectors for a tranpose

--- m4cs/orthogonal_matrices.py
+++ m4cs/orthogonal_matrices.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 from src.operations import *
-from src.properties import *
-from src.vectors import *
-from tests.helper import *
-from m4cs.adjugate_matrices import *
-from m4cs.matrix_inversion import *
 
 #### ORTHOGONAL MATRICES ####
 
 def is_singular(a):
+  m,n = dimensions(a)
+  if m != n:
+    return False
   return determinant(a) == 0
 
 def is_orthogonal(a):
